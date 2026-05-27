992afbd9498ec9198cea93fa61d8a84d354c9d3a
Brendow <brendow_sabe@hotmail.com>
2026-05-27 17:42:51 -0300

    17:42 270526 Adicionado

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#Análise Exploratória da base Varejo
+
+#Importando a biblioteca Pandas para análise dos dados do arquivo.csv
 import pandas as pd
 
 pd.read_csv('Base_Varejo.csv')
 
 print(pd.read_csv('Base_Varejo.csv'))
+
+
+#número de registros
+
+#colunas
+
+#tipos de dados
